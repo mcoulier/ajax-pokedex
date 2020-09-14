861f38739b177bf44bb22ffc5c19d93c991289ac
Michiel Coulier <michielcoulier@gmail.com>
2020-09-14 13:26:59 +0200

    Search gives correct url

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 (()=>{
+    async function fetchPokemon(pokemonId){
+
+        let sear
+    }
     document.getElementById("searchBtn").addEventListener("click", ()=> {
 
-            let userInput = document.getElementById("search").value
-            let url = "https://pokeapi.co/api/v2/pokemon/" + userInput;
-            console.log(userInput)
+            let userInput = document.getElementById("search").value;
+
+        fetch("https://pokeapi.co/api/v2/pokemon/" + userInput )
+            .then(response => response.json())
+            .then(allpokemon => console.log(allpokemon))
 
     })
 
-    fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
-        .then(response => response.json())
-        .then(allpokemon => console.log(allpokemon))
 
 
 })();
