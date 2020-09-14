509916dc491ef8ed9342ff35edd67c1f52abad56
Michiel Coulier <michielcoulier@gmail.com>
2020-09-14 15:40:06 +0200

    search id & image working

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 (()=>{
+
     document.getElementById("searchBtn").addEventListener("click", ()=> {
         let userInput = document.getElementById("search").value;
 
         fetch("https://pokeapi.co/api/v2/pokemon/" + userInput + "/")
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data);
+                document.getElementById("target").innerHTML = `${data.id}`;
+                document.getElementById("img").setAttribute("src", `${data.sprites.front_shiny}`)
+                document.getElementById()
+            });
+
+
+
     })
 
 })();
-
