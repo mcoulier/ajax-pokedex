3313cf70afe9c8f7421485dcd5041e5a59cb832b
Michiel Coulier <michielcoulier@gmail.com>
2020-09-14 15:52:41 +0200

    added name to search

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 (()=>{
 
     document.getElementById("searchBtn").addEventListener("click", ()=> {
+
         let userInput = document.getElementById("search").value;
 
         fetch("https://pokeapi.co/api/v2/pokemon/" + userInput + "/")
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                document.getElementById("target").innerHTML = `${data.id}`;
+                document.getElementById("pokemonID").innerHTML = `${data.id}`;
                 document.getElementById("img").setAttribute("src", `${data.sprites.front_shiny}`)
-                document.getElementById()
+                document.getElementById("pokemonName").innerHTML = `${data.name}`;
             });
 
 
