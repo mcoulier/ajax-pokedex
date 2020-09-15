b8b5073f545c88e81a4ac1bee47c0b14d4f370fd
Michiel Coulier <michielcoulier@gmail.com>
2020-09-15 12:02:38 +0200

    added css pokedex & search, moves, etc working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (()=>{
 
-    document.getElementById("searchBtn").addEventListener("click", ()=> {
+    document.getElementById("blueButton").addEventListener("click", ()=> {
 
         let userInput = document.getElementById("search").value;
         let moveOne = (Math.floor(Math.random() * 40) + 1)
@@ -17,9 +17,9 @@
             .then(response => response.json())
             .then(data => {
                 console.log(data)
-                document.getElementById("pokemonID").innerHTML = data.id;
+                document.getElementById("pokemonID").innerHTML = "ID: " + data.id;
                 document.getElementById("img").setAttribute("src",data.sprites.front_default);
-                document.getElementById("pokemonName").innerHTML = data.name;
+                document.getElementById("pokemonName").innerHTML = "Name: " + data.name;
                 document.getElementById("moveOne").innerHTML = data.moves[moveOne].move.name;
                 document.getElementById("moveTwo").innerHTML = data.moves[moveTwo].move.name;
                 document.getElementById("moveThree").innerHTML = data.moves[moveThree].move.name;
